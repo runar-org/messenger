308e9923e84c7d4fe2c2e94b7fc5a2e2f75dbb00
Kyle Burkett <kyle.burkett@wwinc.com>
2020-07-26 13:31:24 -0500

    add cryptodome library and extend encryption model

--- messenger/models.py
+++ messenger/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Encryption(models.Model):
     key = models.CharField(max_length=500)
+    identifier = models.CharField(max_length=500)
     def __str__(self):
         return self.key
 
--- messenger/views.py
+++ messenger/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from .models import Encryption, Message
+from Cryptodome.Cipher import AES
 
 def index(request):
     return render(request, 'index.html')
@@ -11,13 +12,13 @@ def send(request):
     return render(request, 'send/index.html')
 
 def encrypted(request):
+    obj = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
     message_with_key = {'message': request.POST['message'], 'key_phrase': request.POST['key_phrase']}
     return render(request, 'encrypted.html', message_with_key)
 
-def receive(request, encryption_id=''):
-    # actually store server side without passing to the view
-    # encrypted_message = get_object_or_404(Encryption, key=encryption_id)
-    encryption_message = {'encryption': encryption_id}
+def receive(request, encryption_id='mASuc23'):
+    e = Encryption.objects.get(identifier=encryption_id)
+    encryption_message = {'encryption': encryption_id, 'e': e}
     return render(request, 'receive/index.html', encryption_message)
 
 def result(request):
