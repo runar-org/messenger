9c3ca7ac3cb23a9c038417131eb75b906fa83d36
Kyle Burkett <kyle.burkett@wwinc.com>
2020-07-30 17:03:36 -0500

    making some encryption based on input

--- messenger/views.py
+++ messenger/views.py
@@ -4,6 +4,7 @@ from django.template import loader
 from django.urls import reverse
 from .models import Encryption, Message
 from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
 
 def index(request):
     return render(request, 'index.html')
@@ -12,8 +13,17 @@ def send(request):
     return render(request, 'send/index.html')
 
 def encrypted(request):
-    obj = AES.new('This is a key123', AES.MODE_CBC, 'This is an IV456')
-    message_with_key = {'message': request.POST['message'], 'key_phrase': request.POST['key_phrase']}
+    iv = get_random_bytes(16)
+    message = request.POST['message']
+    key = request.POST['key_phrase']
+    key = key.encode()
+    key = key + (AES.block_size - (len(key) % AES.block_size)) * b'\x00'
+    key = key[:32]
+    message = message.encode()
+    message = message + (AES.block_size - (len(message) % AES.block_size)) * b'\x00'
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    coded = cipher.encrypt(message)
+    message_with_key = {'message': coded.hex(), 'key_phrase': key.hex()}
     return render(request, 'encrypted.html', message_with_key)
 
 def receive(request, encryption_id='mASuc23'):
