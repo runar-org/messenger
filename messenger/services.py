cd06b3c748dde18004731ac7837aee785fae9d78
Kyle Burkett <kyle.burkett@wwinc.com>
2020-08-08 09:36:24 -0500

    Trim leading and trailing whitespaces on user provided (up) keys, as most users would not expect white spaces before and after to be required for the key Fixes: #7

--- messenger/services.py
+++ messenger/services.py
@@ -4,6 +4,7 @@ from Cryptodome.Random import get_random_bytes
 from django.utils import timezone
 from datetime import timedelta
 
+
 # Purpose:  encrypts and saves a message
 # @params:  message: string = the real message we want to encrypt
 #           key: string = the fake message used in decryption process
@@ -18,6 +19,7 @@ def encrypt_message(message, key):
     m = Message(ciphertext=coded, identifier=iv)
     m.save()
     return m
+
 
 # Purpose:  finds a message and decrypts it
 # @params:  identifier: binary = used to find the message to decrypt
@@ -34,14 +36,17 @@ def decrypt_message(identifier, up_key):
     destroy_message(m)  # after message is found and decrypted, delete from db
     return decoded.decode('UTF-8')
 
+
 # Purpose:  takes a user provided key as a string and formats it properly
 # @params:  up_key: string = user provided string as key for decryption
 # @returns: Binary (key for with to encrypt and decrypt)
 def handle_up_key(up_key):
-    key = up_key.encode()
+    key = up_key.strip()  # remove whitespaces
+    key = key.encode()
     key = key + (AES.block_size - (len(key) % AES.block_size)) * b'\x00'
     key = key[:32]  # trim key down to a usable size of 32
     return key
+
 
 # Purpose:  takes a message object and destroys it forever.
 # @params:  message: Message = message object that has just been received
@@ -49,6 +54,7 @@ def handle_up_key(up_key):
 def destroy_message(message):
     message.delete()
     return
+
 
 # Purpose:  looks for messages older than 24 hrs, and destroys them forever.
 # @params:  none
--- messenger/views.py
+++ messenger/views.py
@@ -4,11 +4,14 @@ from django.template import loader
 from django.urls import reverse
 from messenger.services import encrypt_message, decrypt_message
 
+
 def index(request):
     return render(request, 'index.html')
 
+
 def send(request):
     return render(request, 'send/index.html')
+
 
 def encrypted(request):
     message = request.POST['message']
@@ -17,9 +20,11 @@ def encrypted(request):
     generated_url = {'code': m.identifier.hex()}
     return render(request, 'encrypted.html', generated_url)
 
+
 def receive(request, encryption_id='mASuc23'):
     encryption_message = {'e_id': encryption_id}
     return render(request, 'receive/index.html', encryption_message)
+
 
 def result(request):
     encryption_id = request.POST['e_id']
